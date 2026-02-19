3f60a3fad704f9d016089867ea64cff374588342
omarGA423 <161617963+omarGA423@users.noreply.github.com>
2026-02-18 18:15:18 -0600

    if-else
    Primera vez que ocupo estas condicionales.

---
+++ main (4).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main (){
+    
+    int numero,operador=2;
+    
+        cout<<"Digite un numero del 1 al 3:";cin>>numero;
+        
+        if (numero == operador){
+            
+            cout<<"\n Caw le atinaste 😎";
+            
+        }
+        
+        else {
+            
+            cout<<"No le atinaste,suerte 🙌";
+            
+        }
+    return 0;
+}
