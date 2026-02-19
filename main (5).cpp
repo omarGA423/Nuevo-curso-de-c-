a3733e0abb34e4cc29fdc726e0d9f12c8fb57627
omarGA423 <161617963+omarGA423@users.noreply.github.com>
2026-02-18 19:01:45 -0600

    Sentencia switch
    Primera vez ocupando el switch ,con números básicos del 1 al 5.

---
+++ main (5).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main (){
+    
+    int numero;
+    
+        cout<<"Introduzca el numero del 1 al 5: ";cin>>numero;
+        
+            switch(numero){
+                
+                case 1:cout<<"Numero es 1";break;
+                case 2:cout<<"Numero es 2";break;
+                case 3:cout<<"Numero es 3";break;
+                case 4:cout<<"Numero es 4";break;
+                case 5:cout<<"Numero es 5";break;
+                
+                    default:cout<<"El numero no esta en el rango de 1 al 5";
+                
+                
+                
+                
+                
+                
+            }
+    
+    
+    
+    return 0;
+}
