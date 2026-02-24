584c9a7f85792714034a0dcb9ff36af79340888c
omarGA423 <161617963+omarGA423@users.noreply.github.com>
2026-02-24 11:09:04 -0600

    While bucle
    Aquí mi primer contacto con un bucle.

---
+++ main (15).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+        
+        int i ;   //La i es variable tipo entero
+            i=1;   //Aqui asignamos un valor de 1 a mi i ,el valor puede ser cual iostream
+                
+                while (i<=10){   // Aqui digo mientras mi i sea <= a 10
+                                //Lo que esta dentro de mi () es mi condicion
+                    
+                    cout<<i<<endl;  //Si mi i cumple mi condicion entonces pon en mi pantalla a i
+                                    // o sentencia
+                                
+                    
+                    i++; //Aqui le digo a mi programa que avance de 1 en 10
+                         // Avanza de 1 en 1 hasta cumplir mi condicion (i<=10)
+                }
+    
+    return 0;
+}
