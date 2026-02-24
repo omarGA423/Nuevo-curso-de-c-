850419f72cbf69c1eb018aa09921f6a6ff4aba3e
omarGA423 <161617963+omarGA423@users.noreply.github.com>
2026-02-24 11:37:12 -0600

    For bucle
    El iterador puedo poner dentro de mi for (i); es en lo personal más cómodo que el while y el do while.

---
+++ main (16).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+        
+        for (int i=0;i<=10;i++){  //Ahora la diferencia sta en poner el iterador dentro del bulce 
+            
+            cout<<i<<endl;
+        }
+    return 0;
+}
