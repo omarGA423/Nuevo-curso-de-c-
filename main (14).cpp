be6f8fba313934bba7644fc2a210514ea7f3c22e
omarGA423 <161617963+omarGA423@users.noreply.github.com>
2026-02-24 11:01:06 -0600

    do while "Primero actua luego piensa"
    Eldo while es lo mismo que el while solo que este primero actua y luego piensa Do/while,la iteracion "i" va por afuera de mi do,ocupamos lo mismo que el while nomas que el do se encierra con mi sentencia para la pantallita y l ++,nuestra suma consecutiva de i ,la i es  iteracion.

---
+++ main (14).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int i;
+        i= 1;  //aqui tengo mi iteracion afuera
+    do{ //Actua
+        cout<<i<<endl;
+        i++;
+        
+    } while (i <=10);  //"Primero actua y luego piensa XD"
+      //Piensa
+    
+    return 0;
+}
